85bd833e2435a751b04e62bf0672227946d66735
bigox <big_ox@163.com>
2022-03-06 23:32:39 +0800

    获取任务信息接口加入连接名称

--- backend/handler/api/taskApi/getTask.go
+++ backend/handler/api/taskApi/getTask.go
@@ -35,9 +35,28 @@ func GetTaskInfo(ctx *gin.Context) {
 		response.Response(ctx, http.StatusBadRequest, 400, nil, customError.BadRequestError.Error())
 		return
 	}
-	var dbTask taskModel.TaskList
-	err = db.Where("id = ? ", dbTask).First(&dbTask).Error
-	if errors.Is(err, gorm.ErrRecordNotFound) || dbTask.ID == 0 {
+	querySQl := `SELECT
+					t.*,
+					d1.link_name AS source_db_link_name,
+					d1.db_type AS source_db_type,
+					d2.link_name AS target_db_link_name ,
+					d2.db_type AS target_db_type 
+				FROM
+					compare_task_list t
+					LEFT JOIN compare_db_link d1 ON t.source_db_link_id = d1.id
+					LEFT JOIN compare_db_link d2 ON t.target_db_link_id = d2.id
+				where t.id = ?`
+
+	type dbTaskInfo struct {
+		taskModel.TaskList
+		SourceDbLinkName string
+		SourceDbType     string
+		TargetDbLinkName string
+		TargetDbType     string
+	}
+	var taskInfo dbTaskInfo
+	err = db.Raw(querySQl, taskId).Scan(&taskInfo).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) || taskInfo.ID == 0 {
 		response.Response(ctx, http.StatusNotFound, 422, nil, customError.UnprocessableEntityError.Error())
 		return
 	} else if err != nil {
@@ -45,7 +64,7 @@ func GetTaskInfo(ctx *gin.Context) {
 		response.Response(ctx, http.StatusInternalServerError, 500, nil, customError.InternalServerError.Error())
 		return
 	}
-	response.Success(ctx, dbTask, "请求成功")
+	response.Success(ctx, taskInfo, "请求成功")
 }
 
 //
@@ -60,12 +79,29 @@ func GetTaskList(ctx *gin.Context) {
 		response.Response(ctx, http.StatusInternalServerError, 500, nil, customError.InternalServerError.Error())
 		return
 	}
-	var dbTaskList []taskModel.TaskList
-	err = db.Find(&dbTaskList).Error
+	querySQl := `SELECT
+					t.*,
+					d1.link_name AS source_db_link_name,
+					d1.db_type AS source_db_type,
+					d2.link_name AS target_db_link_name ,
+					d2.db_type AS target_db_type 
+				FROM
+					compare_task_list t
+					LEFT JOIN compare_db_link d1 ON t.source_db_link_id = d1.id
+					LEFT JOIN compare_db_link d2 ON t.target_db_link_id = d2.id`
+	type dbTaskInfo struct {
+		taskModel.TaskList
+		SourceDbLinkName string
+		SourceDbType     string
+		TargetDbLinkName string
+		TargetDbType     string
+	}
+	var dbTaskInfoList []dbTaskInfo
+	err = db.Raw(querySQl).Scan(&dbTaskInfoList).Error
 	if err != nil {
 		global.Logger.Error("获取全部任务列表", zap.String("msg", err.Error()))
 		response.Response(ctx, http.StatusInternalServerError, 500, nil, customError.InternalServerError.Error())
 		return
 	}
-	response.Success(ctx, dbTaskList, "获取全部任务列表成功")
+	response.Success(ctx, dbTaskInfoList, "获取全部任务列表成功")
 }
