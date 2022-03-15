4082c69172485cf2748e389c37f92199221260b9
bigox <big_ox@163.com>
2022-03-15 20:41:35 +0800

    结果页初步搭建

--- frontend/src/views/resultTableMsg/Index.vue
+++ frontend/src/views/resultTableMsg/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <el-row :gutter="24" class="watch-row">
-    <el-col :span="16" :offset="4" class="watch-col" >
-      <el-card class="box-card" shadow="never" >
-        <div  class="text item" style="font-size: 13px;color: #5b5b5b">
+    <el-col :span="16" :offset="4" class="watch-col">
+      <el-card class="box-card" shadow="never">
+        <div class="text item" style="font-size: 13px;color: #5b5b5b">
           <span style="margin-right: 10px">选择日期: </span>
           <el-date-picker
               v-model="selectedDate"
@@ -13,14 +13,14 @@
               end-placeholder="结束日期">
           </el-date-picker>
           <span style="margin:0 10px"> 选择任务: </span>
-          <el-select  v-model="selectedTaskId"  size="small"  placeholder="请选择任务">
+          <el-select v-model="selectedTaskId" size="small" placeholder="请选择任务">
             <el-option v-for="item in allTask"
                        :key="item.ID"
                        :label="item.TaskName"
                        :value="item.ID">
             </el-option>
           </el-select>
-          <el-button @click="query()" style="margin:0 30px" type="primary" size="mini"> 查询 </el-button>
+          <el-button @click="query()" style="margin:0 30px" type="primary" size="mini"> 查询</el-button>
         </div>
       </el-card>
       <TablePage :tableData="tableData"></TablePage>
@@ -97,29 +97,41 @@ export default {
           address: '金沙江路 1516 弄'
         }
       ],
-      allTask:[],
+      allTask: [],
     }
   },
   methods: {
-    query(){
-        console.log(this.selectedTaskId)
-        console.log(this.selectedPage)
-        console.log(this.selectedDate)
+    query() {
+      console.log(this.selectedTaskId)
+      console.log(this.selectedPage)
+      console.log(this.selectedDate)
     },
-    getAllTask(){
+    getAllTask() {
       let tasks = storageService.get(storageService.TASK_INFO_LIST)
-      if(tasks.length>0){
+      if (tasks.length > 0) {
         this.allTask = JSON.parse(tasks)
         this.selectedTaskId = this.allTask[0].ID
       }
     },
-    pageChange(currentPage){
+    pageChange(currentPage) {
       this.selectedPage = currentPage
+    },
+    parseDate(dataArray) {
+      let startDate, endDate = ""
+      let startY = dataArray[0].getFullYear()
+      let startM = dataArray[0].getMonth() + 1
+      let startD = dataArray[0].getDate()
+      let endY = dataArray[1].getFullYear()
+      let endM = dataArray[1].getMonth() + 1
+      let endD = dataArray[1].getDate()
+      startDate = `${startY}-${startM}-${startD}`
+      endDate = `${endY}-${endM}-${endD}`
+      return [startDate , endDate]
     }
   },
   mounted() {
     this.getAllTask()
-
+    console.log(this.parseDate(this.selectedDate))
   }
 }
 </script>
@@ -130,19 +142,21 @@ export default {
   /*width: 100%;*/
   height: 100%;
 }
-.watch-col{
+
+.watch-col {
   background-color: #fff;
 }
-.box-card{
+
+.box-card {
   border: none;
 }
 
-.el-pagination{
+.el-pagination {
   margin-top: 20px;
 }
 
 
-#page-body /deep/ .el-input__inner{
+#page-body /deep/ .el-input__inner {
   background-color: #2c3e50;
   height: 100px;
 }
